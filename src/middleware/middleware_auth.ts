908f6829335a47c5f58795831a2c639109088a2f
javiertorres123 <javiiieaea123@gmail.com>
2025-05-15 18:24:50 +0000

    cambioss

--- src/middleware/middleware_auth.ts
+++ src/middleware/middleware_auth.ts
@@ -1,24 +1,44 @@
 import { Request, Response, NextFunction } from "express";
 const jwt  = require ("jsonwebtoken");
 const cookie = require ("cookie");
+const ts = require("ts-pattern")
+const emailregex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const contrasenaregex = /^[a-zA-Z0-9]{8,}$/;
 
+export const VerificarLogin = (req : Request, res: Response, next: NextFunction) => {
+   // si no existe el email o la contraseña, se verifica si existe la cookie
+    const {email, contrasena} = req.body;
+    const result = ts.match(req.body)
+        .with({email: ts.string, contrasena: ts.string}, () => {
+            
+            // si el email no es valido, se retorna un error
+            if (!emailregex.test(email)) {
+                throw new Error("El email no es valido");
+            }
 
-export const veryfylogin = (req : Request, res: Response, next: NextFunction) => {
-    // revisar si la peticion contiene un email y un password
-    const [email, contrasena] = req.body;
-    if (email || contrasena) {
-        if (email) {
-           if (contrasena) {
-               return res.status(201).json({mensaje: "El email y la contraseña son validos."});
-           } else {
-               return res.status(401).json({mensaje: "La contraseña es incorrecta."});
-           }
-        } else {
-            return res.status(401).json({mensaje: "El email es incorrecto."});
-        }
-    } else {
-        
-    }
-}
+            // Si la contraseña no es valida, se retorna un error
+            if (!contrasenaregex.test(contrasena)){
+                throw new Error("la contraseña no es valida");
+            }
+
+            // si existe el email y la contraseña, se verifica si son correctos
+            if (email === process.env.EMAIL && contrasena === process.env.CONTRASENA) {
+                // si son correctos, se genera una token y se guarda en la cookie
+                interface JwtPayload {
+                    email: string;
+                }
+                const token: string = jwt.sign(
+                    { email } as JwtPayload,
+                    process.env.JWT_SECRET as string,
+                    { expiresIn: "4h" }
+                );
+            }
+
+        });
+        .otherwise(() => {
+
+        });
+
+};
 
 

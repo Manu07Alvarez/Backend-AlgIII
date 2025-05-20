bcaafc41fb15c8aae87e2f0e91bb632ad07b7aeb
Manuel Alvarez <Manu_07_alvarez@hotmail.com>
2025-05-20 20:17:08 -0300

    REVISION middleware_auth

--- src/middleware/middleware_auth.ts
+++ src/middleware/middleware_auth.ts
@@ -6,8 +6,9 @@ const contrasenaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$
 
 export const authLogin = (req : Request, res: Response, next: NextFunction) => {
             // la dos estructuras de datos son las siguientes:
-            const {email, contrasena} = req.body;
-            const tsCookie = req.cookies?.TS;
+            const { contrasena } = req.body.contrasena;
+            const { email } = req.body.email;
+            const tsCookie = req.cookies["token"];
 
             // verificar si existe el email y la contraseña en el body
             if (email && contrasena) {
@@ -15,18 +16,20 @@ export const authLogin = (req : Request, res: Response, next: NextFunction) => {
                 if (!emailRegex.test(email)) {throw new Error("El email no es valido")};
                 if (!contrasenaRegex.test(contrasena)) {throw new Error("la contraseña no es valida")};
                 return next();   
-            }
-
-            // si no existe el email o la contraseña, se verifica si existe la cookie
-            if(tsCookie) {
+            }  //REVISION: Verificar casos en el que se encuentre el email o la contraseña pero no ambos
+        
+            if(tsCookie) {  // si no existe el email o la contraseña, se verifica si existe la cookie
                 console.log ("Cookie encontrada.")
                 // verificar si existe la token dentro de la Cookie y si existe la firma de la token
                 try{
                     const decoded = jwt.verify(tsCookie, process.env.JWT_SECRET as string);
                     console.log("token valido: ", decoded);
                     return next()
-                } catch (err) {
-                    return res.status(401).json({mensaje: "Token invalido o expirado"});
+                } catch (err: unknown) {
+                    if (err instanceof Error) {
+                        return res.status(401).json({mensaje: "Token invalido o expirado"});
+                    }
+                    throw err;
                 }
             }
             // si no existe el email, la contraseña o la cookie, se retorna un error

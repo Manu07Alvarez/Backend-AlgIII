25abd2a90a2a9d46526fd21581c8a63b5dc4947e
javiertorres123 <javiiieaea123@gmail.com>
2025-05-20 22:12:17 +0000

    cambios

--- src/middleware/middleware_auth.ts
+++ src/middleware/middleware_auth.ts
@@ -5,32 +5,22 @@ const contrasenaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$
 
 
 export const authLogin = (req : Request, res: Response, next: NextFunction) => {
-            // si no existe el email o la contraseña, se verifica si existe la cookie
-             const {email, contrasena} = req.body;
-             const tsCookie = req.cookies?.TS;
+            // la dos estructuras de datos son las siguientes:
+            const {email, contrasena} = req.body;
+            const tsCookie = req.cookies?.TS;
+
+            // verificar si existe el email y la contraseña en el body
             if (email && contrasena) {
                 console.log("email y contraseñas encontradas")
                 if (!emailRegex.test(email)) {throw new Error("El email no es valido")};
                 if (!contrasenaRegex.test(contrasena)) {throw new Error("la contraseña no es valida")};
-                // se verifica si son correctos
-                if (email === process.env.EMAIL && contrasena === process.env.CONTRASENA) {
-                // si son correctos, se genera una token y se guarda en la cookie
-                interface JwtPayload {
-                    email: string;
-                }
-                const token: string = jwt.sign(
-                    { email } as JwtPayload,
-                    process.env.JWT_SECRET as string,
-                    { expiresIn: "4h" }
-                );
-                res.cookie("TS", token, {httpOnly: true, maxAge: 4 * 60 * 60 * 1000});
-            }
-            return next();   
+                return next();   
             }
 
+            // si no existe el email o la contraseña, se verifica si existe la cookie
             if(tsCookie) {
                 console.log ("Cookie encontrada.")
-                // verificar si existe la token dentro de la Cookie
+                // verificar si existe la token dentro de la Cookie y si existe la firma de la token
                 try{
                     const decoded = jwt.verify(tsCookie, process.env.JWT_SECRET as string);
                     console.log("token valido: ", decoded);
@@ -39,6 +29,6 @@ export const authLogin = (req : Request, res: Response, next: NextFunction) => {
                     return res.status(401).json({mensaje: "Token invalido o expirado"});
                 }
             }
+            // si no existe el email, la contraseña o la cookie, se retorna un error
+            return res.status(401).json({mensaje: "No se han proporcionado credenciales"});
         };
-
-

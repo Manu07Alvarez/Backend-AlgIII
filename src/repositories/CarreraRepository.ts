cf2f1abec35df6923e2ef531194f2a86f1310d85
Manuel Alvarez <Manu_07_alvarez@hotmail.com>
2025-05-27 20:34:37 -0300

    add decorators

--- src/repositories/CarreraRepository.ts
+++ src/repositories/CarreraRepository.ts
@@ -1,19 +1,22 @@
 import { PrismaClient, Carrera } from '../../generated/prisma/client';
+import { validateRepo } from '../decorators/errors/errors';
 
 export class CarreraRepository {
     constructor(
         private readonly carrera: PrismaClient['carrera'],
     ) {}
  
-
+  @validateRepo
   public async create(data: Carrera): Promise<void> {
     await this.carrera.create({data});
   }
 
+  @validateRepo
   public async findAll(): Promise<Carrera[]> {
     return await this.carrera.findMany()
   }
 
+  @validateRepo
   public async findById(id: number): Promise<Partial<Carrera>> {
     return this.carrera.findUniqueOrThrow({
       omit: { 
@@ -23,6 +26,8 @@ export class CarreraRepository {
       where: { id }
     });
   }
+
+  @validateRepo
   public async findByName(nombre: string): Promise<Partial<Carrera>> {
     return this.carrera.findUniqueOrThrow({
     omit: { 

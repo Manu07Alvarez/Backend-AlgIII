54efaacc5eebc838e22518ee11a5da0e7246b701
Manuel Alvarez <manu07284alvarez@gmail.com>
2025-07-22 15:04:08 -0300

    fix: remove validation logic from validateUsuarios middleware and clean up error handling

--- src/middleware/validate_usuarios.ts
+++ src/middleware/validate_usuarios.ts
@@ -6,11 +6,10 @@ export async function validateUsuarios(req: Request, res: Response, next: NextFu
   
     //const usuario = await Prisma.user.findFirst({ where: { email: req.body.email } }); // llamadas a la base de datos en el middleware no
 
-    const { valid, errors } = validateSchema(UsuarioSchema, req.body);
+   
 
-    if (!valid) {
-        console.error("Errores de validación:", errors);
-        return res.status(400).json({ mensaje: "Datos de usuario no válidos", errors });
-    }
+        console.error("Errores de validación:", );
+        return res.status(400).json({ mensaje: "Datos de usuario no válidos"});
+    
     next();
 }

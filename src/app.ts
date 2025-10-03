5fe5d2b0d2427cddd0ffc98dda9a79f416401b5f
javiertorres123 <javiiieaea123@gmail.com>
2025-10-03 18:45:27 -0300

    changes

--- src/app.ts
+++ src/app.ts
@@ -28,4 +28,4 @@ app.use('/', routes);
 
 errlogger.info('🚀 Server started');
 
-app.listen(8001, '0.0.0.0');
+app.listen(5000, '0.0.0.0');
--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -1,30 +1,28 @@
 import { Post } from "../generated/prisma/client.js";
 import { validateService } from "../decorators/errors/errors.js";
 import IPostRepository from "../repositories/interfaces/IPostRepository.js";
-import IPostService  from "./interfaces/IPostService.js";
+import IPostService from "./interfaces/IPostService.js";
 import Service from "./Service.js";
 import { PaginationParams, PaginationResults } from "types/pagination.types.js";
 
 export class PostService extends Service<Post> implements IPostService {
-    constructor (
-        private readonly postRepository: IPostRepository,
-    ) {
+    constructor(private readonly postRepository: IPostRepository) {
         super(postRepository, 'post');
     }
 
     @validateService('not found: ')
-     async findByTitle(name: string): Promise<Partial<Post[]>> {
-         return this.postRepository.findByTitle(name);
-     }
-
-    async getPagination(params: PaginationParams): Promise<PaginationResults<Post>> {
-        const result = await this.postRepository.getPagination(params);
-        const totalPages = Math.ceil(result.total / result.limit);
-        const currentPage = result.page;
-        return {
-            ...result,
-            totalPages,
-            currentPage
-        };
+    async findByTitle(name: string): Promise<Partial<Post[]>> {
+        return this.postRepository.findByTitle(name);
     }
+
+   async getPagination(params: PaginationParams): Promise<PaginationResults<Post>> {
+    const result = await this.postRepository.getPagination(params);
+
+    if (!result || !Array.isArray(result.data)) {
+        throw new Error("Formato de resultado inválido");
+    }
+
+    return result;
+}
+
 }

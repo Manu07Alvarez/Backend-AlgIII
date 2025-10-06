ef986b7ec0e8688389ebfa33dffd54c1ea817b54
javiertorres123 <javiiieaea123@gmail.com>
2025-10-06 13:31:45 -0300

    change

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,6 @@ const tracer = trace.getTracer('app');
 generateAndSaveKeyPair();
 import cors from 'cors';
 import { createProxyMiddleware } from "http-proxy-middleware";
-import { Prisma } from 'db';
 const app = express();
 
 app.use(cors());

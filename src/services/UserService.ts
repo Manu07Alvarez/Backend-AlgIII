e6e042792dd2851b6b9f80f06ea138e7dbd96d7c
javiertorres123 <javiiieaea123@gmail.com>
2025-10-13 16:07:39 -0300

    change

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,15 +1,20 @@
-import { Usuario } from 'db';
 import { UserRepository } from '../repositories/UserRepository.js';
+import { Usuario } from '../generated/prisma/client.js';
 import { SignJWT } from 'jose';
 import { validateService } from '../decorators/errors/errors.js';
 import { getPublicKey } from '../utils/auth/KeyGen.js';
 import { compare } from 'bcrypt-ts';
+import { IUserService } from './interfaces/IUserService.js';
+import Service from './Service.js';
 import { PaginationParams, PaginationResults } from 'types/pagination.types.js';
 
 const publicKey = await getPublicKey();
 
-export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+export class UserService extends Service<Usuario> implements IUserService {
+
+  constructor(
+    private readonly userRepository: UserRepository
+  ) { super(userRepository, 'Usuario'); }
 
   @validateService('not deactivated: ')
   async bajaUsuario(id: number, data: Usuario): Promise<void> {
@@ -20,7 +25,7 @@ export class UserService {
   @validateService('not Logged: ')
   async login(email: string, contraseña: string): Promise<string> {
     const user = await this.userRepository.findByEmail(email);
-    const contraseñaMatch = await compare(contraseña, user.contrasenia as string);
+    const contraseñaMatch = await compare(contraseña, user.contraseña as string);
     if (!contraseñaMatch) {
       throw new Error('Invalid password');
     }
@@ -36,10 +41,4 @@ export class UserService {
   async register(data: Usuario): Promise<void> {
     await this.userRepository.create(data);
   }
-  
-  //FIXME: Tipos diferentes del return al promise
-/*   async getPagination(params: PaginationParams): Promise<PaginationResults<Usuario>> {
-    return this.userRepository.getPagination(params);
-    
-  } */
 }

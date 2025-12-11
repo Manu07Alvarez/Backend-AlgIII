7ec00fe7f9a64e18d150fd2160071ac01514608d
Benjamin Zapata <benjaminzapata1998@gmail.com>
2025-12-11 20:00:06 -0300

    Curriculum corregido

--- src/repositories/CurriculumRepository.ts
+++ src/repositories/CurriculumRepository.ts
@@ -2,8 +2,7 @@ import Repository from "./Repository.js";
 import { PrismaClient, Curriculum } from "db";
 import { validateRepo } from "../decorators/errors/errors.js";
 import iCurriculumRepository from "./interfaces/ICurriculumRepository.js";
-
-const prisma = new PrismaClient();
+import { Constructor } from "typebox";
 
 export default class CurriculumRepository
   extends Repository<Curriculum, "curriculum">
@@ -12,41 +11,9 @@ export default class CurriculumRepository
   constructor() {
     super("curriculum");
   }
-
-  @validateRepo
-  async findAllInUserId(userId: number): Promise<Curriculum[]> {
-    return prisma.curriculum.findMany({
-      where: { id_autor: userId },
-    });
+  findAllInUserId(userId: number): Promise<Curriculum[]> {
+    throw new Error("Method not implemented.");
   }
 
-  @validateRepo
-  async findByName(name: string): Promise<(Curriculum | undefined)[]> {
-    const result = await prisma.curriculum.findMany({
-      where: {
-        nombre: {
-          contains: name,
-          mode: "insensitive",
-        },
-      },
-    });
-
-    // La clase base espera (Curriculum | undefined)[]
-    return result.map((c) => c ?? undefined);
-  }
-
-  @validateRepo
-  async update(id: number, data: Curriculum): Promise<void> {
-    await prisma.curriculum.update({
-      where: { id },
-      data,
-    });
-  }
-
-  @validateRepo
-  async delete(id: number): Promise<void> {
-    await prisma.curriculum.delete({
-      where: { id },
-    });
-  }
 }
+
--- src/services/CurriculumService.ts
+++ src/services/CurriculumService.ts
@@ -1,28 +1,43 @@
 import ICurriculumService from "./interfaces/ICurriculumService.js";
 import { Curriculum } from "db";
 import ICurriculumRepository from "../repositories/interfaces/ICurriculumRepository.js";
-import service from "./Service.js";
+import Service from "./Service.js";
 import { validateService } from "../decorators/errors/errors.js";
-import { GetCurriculumForRolDTO, curriculum_mapper } from "../types/DTOs/CurriculumDTO.js";
-import { toUser } from "../utils/mapper/ForUserRol.js";     
-import { validate } from "@zenstackhq/runtime";
+import {
+  GetCurriculumForRolDTO,
+  curriculum_mapper,
+} from "../types/DTOs/CurriculumDTO.js";
+import { toUser } from "../utils/mapper/ForUserRol.js";
 
-export class CurriculumService extends service<Curriculum> implements ICurriculumService {
-    constructor(
-        private readonly CurriculumRepository: ICurriculumRepository,
-    ) {
-        super(CurriculumRepository, 'Curriculum');
-    } 
-    @validateService('Curriculum not found: ')
-    public async findById(id: number): Promise<GetCurriculumForRolDTO> {
-        const curriculum = await this.CurriculumRepository.findById(id) as Curriculum
-        return await toUser<GetCurriculumForRolDTO, Curriculum, typeof curriculum_mapper>(curriculum, curriculum_mapper);
-    }
-    @validateService('Curriculum not found: ')
-    public async findAllInUserId(userId: number): Promise<GetCurriculumForRolDTO[]> {
-        const curriculum = await this.CurriculumRepository.findAllInUserId(userId) as Curriculum[];
-        return await toUser<GetCurriculumForRolDTO[], Curriculum, typeof curriculum_mapper>(curriculum, curriculum_mapper).then(curriculums => curriculums[0]);
-    }
+export class CurriculumService
+  extends Service<Curriculum>
+  implements ICurriculumService
+{
+  constructor(private readonly CurriculumRepository: ICurriculumRepository) {
+    super(CurriculumRepository, "Curriculum");
+  }
 
+  @validateService("Curriculum not found: ")
+  public async findByName(name: string): Promise<GetCurriculumForRolDTO[]> {
+    const curriculums = (await this.CurriculumRepository.findByName(
+      name
+    )) as Curriculum[];
+
+    return toUser<GetCurriculumForRolDTO, Curriculum, typeof curriculum_mapper>(
+      curriculums as Curriculum[],
+      curriculum_mapper
+    );
+  }
+
+  @validateService("Curriculums not found: ")
+  public async findAllInUserId(
+    userId: number
+  ): Promise<GetCurriculumForRolDTO> {
+    const curriculums = await this.CurriculumRepository.findAllInUserId(userId);
+
+    return toUser<GetCurriculumForRolDTO, Curriculum, typeof curriculum_mapper>(
+      curriculums as Curriculum[],
+      curriculum_mapper
+    ).then((curriculum) => curriculum[0]);
+  }
 }
-
